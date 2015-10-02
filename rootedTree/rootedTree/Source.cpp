fb2e493c93a98710397f99b7fd5e15d504714cee
Gryssim <Wambsu@gmail.com>
2015-10-01 18:23:18 -0600

    New Item rootedTree assignment.
    Additional Reverse Polish changes.

---
+++ rootedTree/rootedTree/Source.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const int INITVAL = 1;
+
+struct treeType {
+	string word;
+	int count;
+	treeType *left, *right;
+};
+
+void createTree(treeType *&root){
+	root = new treeType;
+	
+	root->word = "";
+	root->count = INITVAL;
+	
+	root->left = NULL;
+	root->right = NULL;
+	
+}
+
+bool emptyTree(treeType *root){
+	return root->count < INITVAL;
+}
+
+string cleanup(string word){
+	string dummy;
+	int i = 0;
+	dummy = "";
+	while (i < word.length()){
+		if (word[i] >= 'a' && word[i] <= 'z'){
+			dummy += word[i];
+		}
+		i++;
+	}
+	word = dummy;
+	return word;
+}
+
+
+void insertTree(treeType *root, string word){
+	treeType *knew, *parent, *c;
+	parent = NULL;
+	c = root;
+	if (!emptyTree(root)){
+		while (c != NULL && c->word != word){
+			parent = c;
+			if (word < c->word){
+				c = c->left;
+			}
+			else
+				c = c->right;
+		}
+		if (c != NULL)
+			c->count++;
+		else{
+			knew = new treeType;
+			knew->word = word;
+			knew->count = 1;
+			knew->left = NULL;
+			knew->right = NULL;
+
+
+			if (word < parent->word)
+				parent->left = knew;
+			else
+				parent->right = knew;
+		}
+	}
+	else
+		root->word = word;
+}
+
+void readIn(treeType *root){
+	ifstream inFile("input.dat");
+	string word;
+
+	while (!inFile.eof()){
+		inFile >> word;
+		cleanup(word);
+		insertTree(root, word);
+
+	}
+}
+
+
+void preOrderTrav(treeType *c){
+	cout << c->word;
+	if (c->left != NULL) preOrderTrav(c->left);
+	if (c->right != NULL) preOrderTrav(c->right);
+}
+
+void inOrderTrav(treeType *c){
+	if (c->left != NULL) inOrderTrav(c->left);
+	cout << c->word << " " << c->count << endl;
+	if (c->right != NULL) inOrderTrav(c->right);
+}
+
+void postOrderTrav(treeType *c){
+	if (c->left != NULL) postOrderTrav(c->left);
+	if (c->right != NULL) postOrderTrav(c->right);
+	cout << c->word;
+}
+
+void main(){
+	treeType *root;
+
+	createTree(root);
+	readIn(root);
+
+	inOrderTrav(root);
+}
